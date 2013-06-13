aadd8956c875a120106fb5c036c5db5f932eba76
Nelson Serrano <njserranor@gmail.com>
2013-06-13 00:51:42 -0600

    esqueleto básico del programa
    Menú con:
    Archivo
    -- cargar archivo (falta)
    -- salir
    Opciones (falta)

--- TM3Cintas/TM3Cintas/Form1.h
+++ TM3Cintas/TM3Cintas/Form1.h
@@ -34,7 +34,23 @@ namespace TM3Cintas {
 				delete components;
 			}
 		}
+
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^  archivoToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  cargarArchivoToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  salirToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  opcionesToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  mostrarTMConNotaciónMatemáticaToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  ejecutarTMToolStripMenuItem;
 	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::StatusStrip^  statusStrip1;
+	private: System::Windows::Forms::ToolStripStatusLabel^  toolStripStatusLabel1;
 	protected: 
 
 	private:
@@ -50,34 +66,180 @@ namespace TM3Cintas {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->archivoToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->cargarArchivoToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->salirToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->opcionesToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->mostrarTMConNotaciónMatemáticaToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->ejecutarTMToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->statusStrip1 = (gcnew System::Windows::Forms::StatusStrip());
+			this->toolStripStatusLabel1 = (gcnew System::Windows::Forms::ToolStripStatusLabel());
+			this->menuStrip1->SuspendLayout();
+			this->statusStrip1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {this->archivoToolStripMenuItem, 
+				this->opcionesToolStripMenuItem});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->RenderMode = System::Windows::Forms::ToolStripRenderMode::System;
+			this->menuStrip1->Size = System::Drawing::Size(852, 24);
+			this->menuStrip1->TabIndex = 1;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// archivoToolStripMenuItem
+			// 
+			this->archivoToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {this->cargarArchivoToolStripMenuItem, 
+				this->salirToolStripMenuItem});
+			this->archivoToolStripMenuItem->Name = L"archivoToolStripMenuItem";
+			this->archivoToolStripMenuItem->Size = System::Drawing::Size(60, 20);
+			this->archivoToolStripMenuItem->Text = L"Archivo";
+			// 
+			// cargarArchivoToolStripMenuItem
+			// 
+			this->cargarArchivoToolStripMenuItem->Name = L"cargarArchivoToolStripMenuItem";
+			this->cargarArchivoToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->cargarArchivoToolStripMenuItem->Text = L"Cargar archivo";
+			// 
+			// salirToolStripMenuItem
+			// 
+			this->salirToolStripMenuItem->Name = L"salirToolStripMenuItem";
+			this->salirToolStripMenuItem->Size = System::Drawing::Size(152, 22);
+			this->salirToolStripMenuItem->Text = L"Salir";
+			this->salirToolStripMenuItem->Click += gcnew System::EventHandler(this, &Form1::salirToolStripMenuItem_Click);
+			// 
+			// opcionesToolStripMenuItem
+			// 
+			this->opcionesToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {this->mostrarTMConNotaciónMatemáticaToolStripMenuItem, 
+				this->ejecutarTMToolStripMenuItem});
+			this->opcionesToolStripMenuItem->Name = L"opcionesToolStripMenuItem";
+			this->opcionesToolStripMenuItem->Size = System::Drawing::Size(69, 20);
+			this->opcionesToolStripMenuItem->Text = L"Opciones";
+			// 
+			// mostrarTMConNotaciónMatemáticaToolStripMenuItem
+			// 
+			this->mostrarTMConNotaciónMatemáticaToolStripMenuItem->Name = L"mostrarTMConNotaciónMatemáticaToolStripMenuItem";
+			this->mostrarTMConNotaciónMatemáticaToolStripMenuItem->Size = System::Drawing::Size(278, 22);
+			this->mostrarTMConNotaciónMatemáticaToolStripMenuItem->Text = L"Mostrar TM con notación  matemática";
+			// 
+			// ejecutarTMToolStripMenuItem
+			// 
+			this->ejecutarTMToolStripMenuItem->Name = L"ejecutarTMToolStripMenuItem";
+			this->ejecutarTMToolStripMenuItem->Size = System::Drawing::Size(278, 22);
+			this->ejecutarTMToolStripMenuItem->Text = L"Ejecutar TM";
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(243, 115);
+			this->button1->Location = System::Drawing::Point(15, 212);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Size = System::Drawing::Size(232, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Ejecutar TM";
 			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 63);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(43, 13);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Cinta 1:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(12, 108);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(43, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Cinta 2:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(12, 159);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(43, 13);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"Cinta 3:";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(62, 60);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(185, 20);
+			this->textBox1->TabIndex = 6;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(62, 105);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(185, 20);
+			this->textBox2->TabIndex = 7;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(62, 156);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(185, 20);
+			this->textBox3->TabIndex = 8;
+			// 
+			// statusStrip1
+			// 
+			this->statusStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {this->toolStripStatusLabel1});
+			this->statusStrip1->Location = System::Drawing::Point(0, 296);
+			this->statusStrip1->Name = L"statusStrip1";
+			this->statusStrip1->Size = System::Drawing::Size(852, 22);
+			this->statusStrip1->TabIndex = 9;
+			this->statusStrip1->Text = L"statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this->toolStripStatusLabel1->Name = L"toolStripStatusLabel1";
+			this->toolStripStatusLabel1->Size = System::Drawing::Size(39, 17);
+			this->toolStripStatusLabel1->Text = L"Status";
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(852, 337);
+			this->ClientSize = System::Drawing::Size(852, 318);
+			this->Controls->Add(this->statusStrip1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"Form1";
 			this->Text = L"Form1";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->statusStrip1->ResumeLayout(false);
+			this->statusStrip1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-				 MessageBox::Show("Hola mundo");
+
+	private: System::Void salirToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+				 Application::Exit();
 			 }
-	};
+};
 }
 
